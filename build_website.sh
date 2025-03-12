0235a06314b08e1fc14c1a5278075e932340b9f1
Jonas Pleyer <jonas.dev@pleyer.org>
2025-03-12 19:43:03 +0100

    formatting

--- build_website.sh
+++ build_website.sh
@@ -19,9 +19,7 @@ generate_docs() {
     for target in ${TARGETS[@]}; do
         cargo +nightly-2025-02-01 rustdoc \
             -p $target \
-            --all-features --\
-            --cfg docsrs\
-            --theme $CWD/static/hextra.css \
+            --all-features -- --cfg docsrs --theme $CWD/static/hextra.css \
             --default-theme hextra \
             --html-in-header $CWD/custom_navbar.html \
             --html-in-header $CWD/.docs-header.html
@@ -40,30 +38,30 @@ build_website() {
 movie() {
     # Generate the movie
     gource \
-    -s .15 \
-    --default-user-image static/JonasPleyer-circle.png \
-    -1280x720 \
-    --auto-skip-seconds .05 \
-    --multi-sampling \
-    --stop-at-end \
-    --key \
-    --highlight-users \
-    --date-format "%d/%m/%y" \
-    --hide mouse,filenames \
-    --file-idle-time 0 \
-    --max-files 0  \
-    --background-colour 000000 \
-    --font-size 25 \
-    --output-framerate 60 \
-    --disable-auto-rotate \
-    --max-user-speed 30 \
-    --dir-name-depth 1 \
-    --highlight-dirs \
-    --dir-name-position 1.0 \
-    --file-filter "(cellular_raza-homepage|cellular_raza_book|cellular_raza-book|cellular_raza-benchmarks|benchmark_results)" \
-    --output-ppm-stream - \
-    cellular_raza \
-    | ffmpeg -y -r 60 -f image2pipe -pix_fmt yuv420p -c:v h264 -y -vcodec ppm -i - -b 65536K output.mp4
+        -s .15 \
+        --default-user-image static/JonasPleyer-circle.png \
+        -1280x720 \
+        --auto-skip-seconds .05 \
+        --multi-sampling \
+        --stop-at-end \
+        --key \
+        --highlight-users \
+        --date-format "%d/%m/%y" \
+        --hide mouse,filenames \
+        --file-idle-time 0 \
+        --max-files 0 \
+        --background-colour 000000 \
+        --font-size 25 \
+        --output-framerate 60 \
+        --disable-auto-rotate \
+        --max-user-speed 30 \
+        --dir-name-depth 1 \
+        --highlight-dirs \
+        --dir-name-position 1.0 \
+        --file-filter "(cellular_raza-homepage|cellular_raza_book|cellular_raza-book|cellular_raza-benchmarks|benchmark_results)" \
+        --output-ppm-stream - \
+        cellular_raza |
+        ffmpeg -y -r 60 -f image2pipe -pix_fmt yuv420p -c:v h264 -y -vcodec ppm -i - -b 65536K output.mp4
     # Compress the movie
     ffmpeg -i output.mp4 -pix_fmt yuv420p -y cellular_raza-development-gource.mp4
     # Move it to the hompage folder
@@ -84,7 +82,7 @@ scc_table() {
 }
 
 todo_table() {
-    ./static/internals/todo-table-generate.sh > ./static/internals/todo-table.html
+    ./static/internals/todo-table-generate.sh >./static/internals/todo-table.html
 }
 
 usage() {
@@ -106,7 +104,7 @@ upload() {
     # id does not filter duplicates and thus takes a long time
     # scp -r public_html celluld@www139.your-server.de:/
 
-    sftp celluld@www139.your-server.de<<EOF
+    sftp celluld@www139.your-server.de <<EOF
     put -R $cwd/public_html
     exit
 EOF
@@ -116,41 +114,40 @@ EOF
 handle_options() {
     while [ $# -gt 0 ]; do
         case $1 in
-            -d | --doc)
-                generate_docs
-                ;;
-            -w | --website)
-                build_website
-                ;;
-            -u | --upload)
-                upload
-                ;;
-            -m | --movie)
-                movie
-                ;;
-            --scc)
-                scc_table
-                ;;
-            --todo)
-                todo_table
-                ;;
-            -a | --all)
-                generate_docs
-                build_website
-                scc_table
-                todo_table
-                upload
-                ;;
-            -h | --help)
-                usage
-                ;;
-            \?)
-                usage
-                ;;
+        -d | --doc)
+            generate_docs
+            ;;
+        -w | --website)
+            build_website
+            ;;
+        -u | --upload)
+            upload
+            ;;
+        -m | --movie)
+            movie
+            ;;
+        --scc)
+            scc_table
+            ;;
+        --todo)
+            todo_table
+            ;;
+        -a | --all)
+            generate_docs
+            build_website
+            scc_table
+            todo_table
+            upload
+            ;;
+        -h | --help)
+            usage
+            ;;
+        \?)
+            usage
+            ;;
         esac
         exit 0
     done
 }
 
 handle_options "$@"
-
